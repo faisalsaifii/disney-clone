aced84804a8b052fe78e373f7a9419d0cee570ad
faisalsaifii <m.faisal.s@outlook.com>
2022-10-03 16:09:41 +0530

    Switch replaced with Routes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Login from './components/Login';
 
@@ -6,11 +6,11 @@ function App() {
   return (
     <div className="App">
       <Router>
-        <Switch>
+        <Routes>
           <Route exact path="/">
             <Login/>
           </Route>
-        </Switch>
+        </Routes>
       </Router>
     </div>
   );
