b229f7f0978c7775b0eb4e64058b62659d8a2843
faisalsaifii <m.faisal.s@outlook.com>
2022-10-03 15:59:52 +0530

    Routing Set-up

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
 import Login from './components/Login';
 
 function App() {
   return (
     <div className="App">
-      <Login/>
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <Login/>
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
